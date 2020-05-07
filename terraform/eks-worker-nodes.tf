e7dd8d3b2bcb9f947fb207ffd798065b900d2cf4
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2020-05-07 17:55:01 +0300

    Key name as variable

--- terraform/eks-worker-nodes.tf
+++ terraform/eks-worker-nodes.tf
@@ -39,7 +39,7 @@ resource "aws_eks_node_group" "demo" {
   subnet_ids      = [aws_subnet.public-subnet.id,aws_subnet.private-subnet.id]
 
   remote_access {
-    ec2_ssh_key = "sorin_key"
+    ec2_ssh_key = var.key_name
     source_security_group_ids = [aws_security_group.sgweb.id]
   }
 
