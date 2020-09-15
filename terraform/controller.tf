058b63bcff6f14f544201be80083c48f083a7c8d
Sorin Boiangiu <sorinboia@gmail.com>
2020-09-15 12:54:36 +0300

    Config

--- terraform/controller.tf
+++ terraform/controller.tf
@@ -77,7 +77,7 @@ resource "aws_instance" "controller" {
       swapoff -a
       ufw disable
       apt-get install jq socat conntrack -y
-      wget https://sorinnginx.s3.eu-central-1.amazonaws.com/controller-installer-3.7.0.tar.gz -O /home/ubuntu/controller.tar.gz
+      wget https://sorinnginx.s3.eu-central-1.amazonaws.com/offline-controller-installer-3.8.tar.gz -O /home/ubuntu/controller.tar.gz
       tar zxvf /home/ubuntu/controller.tar.gz -C /home/ubuntu/
       host_ip=$(curl -s ifconfig.me)
       export HOME=/home/ubuntu
